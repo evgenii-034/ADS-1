8904a030f1241929df936dc41a9bd745c1eb5623
evgenii-034 <99119985+evgenii-034@users.noreply.github.com>
2022-02-26 14:38:22 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -2,15 +2,12 @@
 #include <cstdint>
 #include "alg.h"
 bool checkPrime(uint64_t value) {
-  if (value > 1)
-    {
-
-        for (uint64_t i = 2; i < value; i++)
-            if (value % i == 0)
-                return false;
-
-
-        return true;
+  if (value > 1) {
+     for (uint64_t i = 2; i < value; i++) {
+       if (value % i == 0)
+       return false;
+     }
+      return true;
     }
     else
         return false;
@@ -22,49 +19,29 @@ uint64_t nPrime(uint64_t n) {
     for (uint64_t i = 2; ; ++i) {
 
         if (checkPrime(i)) {
-
             ++b;
-
-            if (b == n) {
-
-                return i;
+           if (b == n) {
+            return i;
             }
-
-
-        }
-        
-
-
-    }
+ }
+ }
 // вставьте код функции
 }
-
 uint64_t nextPrime(uint64_t value) {
   for (uint64_t i = value + 1; ; ++i) {
-
-
-        if (checkPrime(i)) {
-
-            return i;
+   if (checkPrime(i)) {
+    return i;
         }
-
-    }
- 
-// вставьте код функции
+  }
+ // вставьте код функции
 }
-
 uint64_t sumPrime(uint64_t hbound) {
    uint64_t summ = 0;
-
-    for (uint64_t i = 0; i < hbound; ++i){
-
-        if (checkPrime(i))  {
-
-            summ += i;
+for (uint64_t i = 0; i < hbound; ++i) {
+if (checkPrime(i))  {
+ summ += i;
         }
-
-    }
-
-    return summ;
+}
+return summ;
 // вставьте код функции
 }
