8c39bf8d5ec742a1026f89fc94ad9c419e298cd4
evgenii-034 <99119985+evgenii-034@users.noreply.github.com>
2022-02-26 13:52:58 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,17 +4,69 @@
 
 
 bool checkPrime(uint64_t value) {
+  if (value > 1)
+    {
+
+        for (uint64_t i = 2; i < value; i++)
+            if (value % i == 0)
+                return false;
+
+
+        return true;
+    }
+    else
+        return false;
 // вставьте код функции
 }
 
 uint64_t nPrime(uint64_t n) {
+  int b = 0;
+    for (uint64_t i = 2; ; ++i) {
+
+        if (checkPrime(i)) {
+
+            ++b;
+
+            if (b == n) {
+
+                return i;
+            }
+
+
+        }
+        
+
+
+    }
 // вставьте код функции
 }
 
 uint64_t nextPrime(uint64_t value) {
+  for (uint64_t i = value + 1; ; ++i) {
+
+
+        if (checkPrime(i)) {
+
+            return i;
+        }
+
+    }
+ 
 // вставьте код функции
 }
 
 uint64_t sumPrime(uint64_t hbound) {
+   uint64_t summ = 0;
+
+    for (uint64_t i = 0; i < hbound; ++i){
+
+        if (checkPrime(i))  {
+
+            summ += i;
+        }
+
+    }
+
+    return summ;
 // вставьте код функции
 }
